e6bca32efa05353b20b7b93268086937aa3e28de
Variniia Zavadskaia <variniyaal@gmail.com>
2024-10-02 11:05:55 +0300

    First commit

---
+++ cmps/AppHeader.jsx
@@ -0,0 +1,25 @@
+const { Link, NavLink } = ReactRouterDOM
+
+export function AppHeader() {
+    return (
+        <header className="app-header">
+            <div className="logo">
+                <Link to="/" className="logo-link">LOGO</Link>
+            </div>
+            <nav className="nav-links">
+                <NavLink to="/" className="nav-link">
+                    Home
+                </NavLink>
+                <NavLink to="/about" className="nav-link">
+                    About
+                </NavLink>
+                <NavLink to="/mail" className="nav-link">
+                    Mail
+                </NavLink>
+                <NavLink to="/note" className="nav-link">
+                    Note
+                </NavLink>
+            </nav>
+        </header>
+    );
+}
