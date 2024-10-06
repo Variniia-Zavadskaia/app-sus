3ca25afcca04df02a6a6698c6d1ef4243972e3e5
Variniia Zavadskaia <variniyaal@gmail.com>
2024-10-06 22:55:21 +0300

    Fix css

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -1,16 +1,22 @@
 import { NoteTxt } from "./dynamic-inputs/NoteTxt.jsx";
 import { NoteImg } from "./dynamic-inputs/NoteImg.jsx";
 import { NoteTodos } from "./dynamic-inputs/NoteTodos.jsx";
-import { NotePrevFooter } from "./NotePrevFooter.jsx";
+import { ColorInput } from "../../note/cmps/dynamic-inputs/ColorInput.jsx"
+// import { NotePrevFooter } from "./NotePrevFooter.jsx";
 
 const { useRef, useState } = React
 const { Outlet, NavLink } = ReactRouterDOM
 const { Link } = ReactRouterDOM
 
-
-
 export function NotePreview({ note, onRemoveNote }) {
 
+    const [noteStyle, setNoteStyle] = useState({
+        backgroundColor: '#101010',
+    })
+
+    function onSetNoteStyle(newStyle) { // { backgroundColor: 'royalblue }
+        setNoteStyle(prevStyle => ({ ...prevStyle, ...newStyle }))
+    }
     // const [isPinned, setIsPinned] = useState(false);
     // const handlePin = () => {
     //     setIsPinned(!isPinned);
@@ -18,12 +24,12 @@ export function NotePreview({ note, onRemoveNote }) {
 
 
 
-    const handleFocusOut = (event) => {
-        let relatedTarget = event.relatedTarget;
-        if (!relatedTarget) {
-            setShowTitle(false);
-        }
-    };
+    // const handleFocusOut = (event) => {
+    //     let relatedTarget = event.relatedTarget;
+    //     if (!relatedTarget) {
+    //         setShowTitle(false);
+    //     }
+    // };
 
     function onChangeInfo(params) {
 
@@ -31,22 +37,22 @@ export function NotePreview({ note, onRemoveNote }) {
     // const height = Math.floor(100 + Math.random() * 500);
 
     return (
-        <article onBlur={handleFocusOut} className="note-preview note" >
+        <article className="note-preview note" >
             {/* <div className="note-inner" style={{ height: `${height}px` }}> */}
 
-                <DynamicCmp cmpType={note.type} info={note.info} onChangeInfo={onChangeInfo} />
+            <DynamicCmp cmpType={note.type} info={note.info} onChangeInfo={onChangeInfo} />
 
-                {/* <section className="active-btn"> */}
-                {/* <NotePrevFooter /> */}
-                {/* </section> */}
+            {/* <section className="active-btn"> */}
+            {/* <NotePrevFooter /> */}
+            {/* </section> */}
 
-                <section className="active-btn">
-                    <button className="btn"><i className="fa-solid fa-thumbtack"></i></button>
-                    <button className="btn"><i className="fa-solid fa-palette"></i></button>
-                    <button className="btn"><i className="fa-solid fa-envelope"></i></button>
-                    <Link to={`/note/edit/${note.id}`}><button className="btn"><i className="fa-solid fa-pen-to-square"></i></button></Link>
-                    <button className="btn" onClick={() => onRemoveNote(note.id)}><i className="fa-solid fa-trash-can"></i></button>
-                </section>
+            <section className="active-btn">
+                <button className="btn"><i className="fa-solid fa-thumbtack"></i></button>
+                <ColorInput  {...noteStyle} name="Lala" onSetNoteStyle={onSetNoteStyle}/>
+                <button className="btn"><i className="fa-solid fa-envelope"></i></button>
+                <Link to={`/note/edit/${note.id}`}><button className="btn"><i className="fa-solid fa-pen-to-square"></i></button></Link>
+                <button className="btn" onClick={() => onRemoveNote(note.id)}><i className="fa-solid fa-trash-can"></i></button>
+            </section>
             {/* </div> */}
         </article>
     )
--- apps/note/cmps/dynamic-inputs/ColorInput.jsx
+++ apps/note/cmps/dynamic-inputs/ColorInput.jsx
@@ -1,5 +1,12 @@
-export function ColorInput({ name, onSetNoteStyle, backgroundColor }) {
+const { useState, useRef } = React
 
+export function ColorInput({ name, onSetNoteStyle, backgroundColor }) {
+ console.log('DDDFFDDFFDD');
+ 
+    const [color, setColor] = useState('#fff');
+    const [showPicker, setShowPicker] = useState(false);
+    const pickerRef = useRef(null);
+  
     const colors = [
         '#F44236',
         '#9C27B0',
@@ -14,10 +21,28 @@ export function ColorInput({ name, onSetNoteStyle, backgroundColor }) {
         onSetNoteStyle(newStyle)
     }
 
+    function handleFocusOut(event) {
+        let relatedTarget = event.relatedTarget;
+        if (!relatedTarget) {
+            setShowPicker(false);
+        }
+    };
+
+    const handleColorChange = (color) => {
+      setColor(color.hex);
+    };
+  
+    function togglePicker() {
+        console.log('PPPPPPPPPPP');
+        
+      setShowPicker(!showPicker);
+    };
+  
     return (
-        <section className="color-input">
-            <div className="items-container">
-                {colors.map(color => (
+      <div onBlur={handleFocusOut} >
+         <button className="btn" onClick={() => setShowPicker(true)}><i className="fa-solid fa-palette"></i></button>
+          <div className="color-container" ref={pickerRef} style={{ display: showPicker ? '' : 'none' }}>
+             {colors.map(color => (
                     <div
                         key={color}
                         className={`item ${color === backgroundColor ? 'chosen' : ''}`}
@@ -25,9 +50,51 @@ export function ColorInput({ name, onSetNoteStyle, backgroundColor }) {
                         onClick={() => onSetColor(color)}
                     ></div>
                 ))}
-            </div>
-            
-            <h3>Hello {name}!, pick a color!</h3>
-        </section >
-    )
+          </div>
+      </div>
+    );
+
+/*
+    const [showTitle, setShowTitle] = useState(false);
+    const titleRef = useRef(null);
+
+    const handleFocusOut = (event) => {
+        let relatedTarget = event.relatedTarget;
+        if (!relatedTarget) {
+            setShowTitle(false);
+        }
+    };
+    */
+
+    return (
+        <div className="add-note">
+            <form className="note-form" onBlur={handleFocusOut}>
+                <div className="note-show" >
+                    <input
+                        id="title"
+                        ref={titleRef}
+                        style={{ display: showTitle ? '' : 'none' }}
+                        placeholder="Title"
+                    />
+                </div>
+                <div className="note-show" >
+                    <input
+                        id="content"
+                        onFocus={() => setShowTitle(true)}
+                        placeholder="Take a note..."
+                    />
+                    <div className="note-cmpn">
+                        <button className="btn-cmpn" ><i className="fa-regular fa-square-check"></i></button>
+                        <button className="btn-cmpn" ><i className="fa-solid fa-image"></i></button>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    );
 }
+
+
+
+
+
