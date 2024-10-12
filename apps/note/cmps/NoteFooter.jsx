e0086154803cabe2e1fcd552bfaa70b28b01acf3
Variniia Zavadskaia <variniyaal@gmail.com>
2024-10-12 21:14:12 +0300

    Create mail query for noteTxt

--- apps/note/cmps/NoteFooter.jsx
+++ apps/note/cmps/NoteFooter.jsx
@@ -1,9 +1,11 @@
-const { useState} = React
+const { useState } = React
+const { useNavigate } = ReactRouterDOM
 import { ColorInput } from "../../note/cmps/dynamic-inputs/ColorInput.jsx"
 
-export function NoteFooter({ note, onRemoveNote, onEditNote, onSaveNote}) {
+export function NoteFooter({ note, onRemoveNote, onEditNote, onSaveNote }) {
 
     const [noteStyle, setNoteStyle] = useState(note.style || { backgroundColor: '#e8f0fe' })
+    const navigate = useNavigate()
 
     function onSetNoteStyle(newStyle) {
         const updatedStyle = { ...noteStyle, ...newStyle };
@@ -16,43 +18,52 @@ export function NoteFooter({ note, onRemoveNote, onEditNote, onSaveNote}) {
         note.isPinned = !note.isPinned
         onSaveNote(note)
     }
-   
+
+    function handleSendToMail() {
+        if (note.type === 'NoteTxt') {
+            const title = encodeURIComponent(note.info.title);
+            const body = encodeURIComponent(note.info.txt);
+            // Redirect to MissKeep with query params
+            navigate(`/mail/?title=${title}&body=${body}`);
+        }
+    };
+
     return (
         <footer className="note-footer">
             {/* <section className="active-btn"> */}
-                <div className="icons">
-                    <div className="tooltip">
-                        <button className="icon-button" onClick={onTogglePin} aria-label={`${note.isPinned ? 'Unpin' : 'Pin'} `}>
-                            <i className={`fa-solid ${note.isPinned ? 'fa-thumbtack-slash' : 'fa-thumbtack'} `}></i>
-                        </button>
-                        <span className="tooltip-text">{`${note.isPinned ? 'Unpin' : 'Pin'} `}</span>
-                    </div>
-                    <div className="tooltip">
-                        <ColorInput className="icon-button" onSetNoteStyle={onSetNoteStyle} currentColor={noteStyle.backgroundColor} aria-label="Background" />
-                        <span className="tooltip-text">Background Color</span>
-                    </div>
-                    <div className="tooltip">
-                        <button className="icon-button" aria-label="Send">
-                            <i className="fa-solid fa-envelope"></i>
-                        </button>
-                        <span className="tooltip-text">Send</span>
-                    </div>
-                    {onEditNote !== null && 
+            <div className="icons">
+                <div className="tooltip">
+                    <button className="icon-button" onClick={onTogglePin} aria-label={`${note.isPinned ? 'Unpin' : 'Pin'} `}>
+                        <i className={`fa-solid ${note.isPinned ? 'fa-thumbtack-slash' : 'fa-thumbtack'} `}></i>
+                    </button>
+                    <span className="tooltip-text">{`${note.isPinned ? 'Unpin' : 'Pin'} `}</span>
+                </div>
+                <div className="tooltip">
+                    <ColorInput className="icon-button" onSetNoteStyle={onSetNoteStyle} currentColor={noteStyle.backgroundColor} aria-label="Background" />
+                    <span className="tooltip-text">Background Color</span>
+                </div>
+                <div className="tooltip">
+                    <button className="icon-button"  onClick={handleSendToMail} aria-label="Send">
+                        <i className="fa-solid fa-envelope"></i>
+                    </button>
+                    <span className="tooltip-text">Send to Mail</span>
+                </div>
+                {onEditNote !== null &&
                     <div className="tooltip">
                         <button className="icon-button" onClick={() => onEditNote(note)} aria-label="Edit">
                             <i className="fa-solid fa-pen-to-square"></i>
                         </button>
                         <span className="tooltip-text">Edit</span>
                     </div>}
-                    {onRemoveNote !== null &&
+                {onRemoveNote !== null &&
                     <div className="tooltip">
                         <button className="icon-button" onClick={() => onRemoveNote(note.id)} aria-label="Remove">
                             <i className="fa-solid fa-trash-can"></i>
                         </button>
                         <span className="tooltip-text">Remove</span>
                     </div>}
-                </div>
-                {/* <button className="close-btn" type="button">
+            </div>
+            {/* <button className="close-btn" type="button">
                     Close
                 </button> */}
             {/* </section> */}
