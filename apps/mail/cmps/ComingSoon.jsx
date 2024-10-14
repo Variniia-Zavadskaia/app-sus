5d6d9cf5839f564880d154815b21b054813a07ac
supaGirl <kerenvasserman.bm@gmail.com>
2024-10-14 21:41:33 +0300

    Fix bugs

--- apps/mail/cmps/ComingSoon.jsx
+++ apps/mail/cmps/ComingSoon.jsx
@@ -1,9 +1,20 @@
+const {useNavigate} = ReactRouterDOM
+
 export function ComingSoon() {
+  const navigate = useNavigate()
+
+  function onBack() {
+    navigate('/mail')
+  }
   return (
-    <section className="container coming-soon">
+    <section className="coming-soon ">
+      <button className="go-back-to-site" onClick={onBack}>Go back</button>
+    <div className="container coming-soon">
+
       <h1>This site is under construction</h1>
 
       <h2>Coming Soon.....</h2>
+    </div>
     </section>
   )
 }
--- apps/mail/cmps/MailHeader.jsx
+++ apps/mail/cmps/MailHeader.jsx
@@ -8,6 +8,11 @@ export function MailHeader({filterBy, onSetFilterBy, openMenu, filteredMails ,up
   function goMailIndexPage(){
     navigate('/mail')
   }
+
+  function navToComingSoon() {
+    navigate(`/mail/underConstruction`)
+  }
+
   return (
     <div className="mail-header-app">
       <div className="header-nav-logo">
@@ -28,12 +33,12 @@ export function MailHeader({filterBy, onSetFilterBy, openMenu, filteredMails ,up
       </div>
       <div className="mail-filter-container ">
         <MailFilter filterBy={filterBy} onSetFilterBy={onSetFilterBy} mails={filteredMails} updateMailStatus={updateMailStatus}/>
-        <i className="fa-regular fa-circle-question" title="Support"></i>
-        <i className="fa-solid fa-gear" title="Settings"></i>
+        <i className="fa-regular fa-circle-question" title="Support" onClick={navToComingSoon}></i>
+        <i className="fa-solid fa-gear" title="Settings" onClick={navToComingSoon}></i>
       </div>
 
       <div className="user-action">
-        <i className="fa-solid fa-grip" title="App"></i>
+        <i className="fa-solid fa-grip" title="App" onClick={navToComingSoon}></i>
         <div className="user-img-container">
           <img src="https://via.placeholder.com/150" alt="user-img" className="user-img" />
         </div>
