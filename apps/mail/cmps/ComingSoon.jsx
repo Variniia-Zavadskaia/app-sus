4641d16a53d0c2da44a921202a5f810907fd53c0
supaGirl <kerenvasserman.bm@gmail.com>
2024-10-15 15:52:06 +0300

    Add animateCSS to coming soon cmp

--- apps/mail/cmps/ComingSoon.jsx
+++ apps/mail/cmps/ComingSoon.jsx
@@ -1,20 +1,33 @@
+import { animateCSS } from "../../../services/util.service.js"
+
 const {useNavigate} = ReactRouterDOM
+const {useEffect, useRef} = React
 
 export function ComingSoon() {
   const navigate = useNavigate()
 
+  const titleRef = useRef(null)
+  const subtitleRef = useRef(null)
+
+  useEffect(() => {
+    // Trigger animation on load
+    animateCSS(titleRef.current, 'bounceIn')
+    animateCSS(subtitleRef.current, 'fadeInUp')
+  }, [animateCSS])
+
   function onBack() {
-    navigate('/mail')
+    navigate('/')
   }
+
   return (
-    <section className="coming-soon ">
-      <button className="go-back-to-site" onClick={onBack}>Go back</button>
-    <div className="container coming-soon">
-
-      <h1>This site is under construction</h1>
-
-      <h2>Coming Soon.....</h2>
-    </div>
+    <section className="coming-soon">
+      <button className="go-back-to-site" onClick={onBack}>
+        Go Back
+      </button>
+      <div className="container coming-soon-content">
+        <h1 ref={titleRef} className="coming-soon-title">This site is under construction</h1>
+        <h2 ref={subtitleRef} className="coming-soon-subtitle">Coming Soon...</h2>
+      </div>
     </section>
   )
 }
--- services/util.service.js
+++ services/util.service.js
@@ -153,3 +153,19 @@ export const formatTimeAgo = (dateString) => {
 }
 
 export  const appLabels=['Critical', 'Family', 'Work','Friends', 'Spam', 'Memories', 'Romantic']
+
+export function animateCSS(el, animation = '', isRemoveClass = true) {
+  const prefix = 'animate__'
+  return new Promise((resolve, reject) => {
+      const animationName = `${prefix}${animation}`
+      el.classList.add(`${prefix}animated`, animationName)
+
+      function handleAnimationEnd(event) {
+          event.stopPropagation()
+          if (isRemoveClass) el.classList.remove(`${prefix}animated`, animationName)
+          resolve('Animation ended')
+      }
+
+      el.addEventListener('animationend', handleAnimationEnd, { once: true })
+  })
+}
